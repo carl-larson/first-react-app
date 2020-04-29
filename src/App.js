d50d6014e6d734b334209660f84a82febd646ff7
carl-larson <clarson08@gmail.com>
2020-04-28 20:49:22 -0500

    todo list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor (props) {
     super (props)
     this.state = {
-      isOn: false
+      input: '',
+      items: []
     }
   }
+  toggle = () => {
+    
+    // this.setState({isOn: !this.state.isOn})
+  }
+
+  inputUpdate = event => {
+    this.setState({input: event.target.value})
+  }
+  
+  formSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      items: [...this.state.items, this.state.input],
+      input: ''
+    })
+  }
+
   render() {
+    // console.log(this.state.isOn)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>TEST</h1>
+          <h1>Little Todo List</h1>
+          <form onSubmit={this.formSubmit}>
+            <input type="text" placeholder="TODO Item" value={this.state.input} onChange={this.inputUpdate}></input>
+            <button>Submit</button>
+          </form>
+          <ul>
+            {this.state.items.map((item, index) => {
+              return <li key={index}>{this.state.items[index]}</li>
+            })}
+          </ul>
         </header>
       </div>
     );
